f840e3524b06d41938bce684583ca35584bcf70f
Talha Imran <talhaimran284@gmail.com>
2020-06-19 10:14:49 +0500

    Half Way Through the game

--- Web Projects/TicTacToe/js/script.js
+++ Web Projects/TicTacToe/js/script.js
@@ -1,4 +1,55 @@
-function placeSomeThing(placeHolder){
-    var x = "X";
-    placeHolder.textContent = x;
+// Picking up Random Square
+
+var squares = document.getElementsByClassName("board__square");
+console.log(squares);
+// Two Players O Ai and X Human
+var aiBot = "X";
+var human = "O";
+var currentPlayer = aiBot;
+
+// Game Board
+
+
+var board = [['','',''],
+             ['','',''],
+             ['','','']];            
+
+function placeSomeThing(myChoice){
+    myChoice.textContent = currentPlayer;
+    switchPlayer();
+}
+
+function startGame(){
+    if(currentPlayer == aiBot){
+        aiChoice  = squares[Math.floor(Math.random()*squares.length)]
+        aiChoice.textContent = currentPlayer;
+        switchPlayer();
+    }
+}
+
+function switchPlayer(){
+    if(currentPlayer == aiBot){
+        currentPlayer = human;
+    }
+    else {
+        currentPlayer = aiBot;
+    }
+}
+
+/*
+while gameIsNotFinised(){
+    playerToTakeTurn();
+    check(he won or not){
+        gameEnds();
+    }
+
+    playerToTakeTurn();
+}*/
+
+function startGame(){
+    for(let i = 0; i < players.length; i++){
+        if(i % 2 == 0 ){
+
+        }
+    }
 }
