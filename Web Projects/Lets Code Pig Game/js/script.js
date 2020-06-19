671f5ba2fd06f963cf6c09e0645e293948fedc08
Talha Imran <talhaimran284@gmail.com>
2020-06-19 13:57:00 +0500

    Started to give functionality to the game

--- Web Projects/Lets Code Pig Game/js/script.js
+++ Web Projects/Lets Code Pig Game/js/script.js
@@ -1,8 +1,11 @@
 //alert("Pig Game");
 var scores = [0, 0];
 var currentScore = 0;
-
+var diceScore = undefined;
 
 function rollDice(){
-
+    diceScore = Math.floor(Math.random() * 6) + 1;
+    currentScore += diceScore;
+    var consoleIt = document.querySelector("#round-score--player-0");
+    consoleIt.textContent = currentScore;
 }
